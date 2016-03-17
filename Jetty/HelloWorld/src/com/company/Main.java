653243112fcc4f5d5898e5703ed0fd5112c81963
Mahmood <ramzani.mahmood@gmail.com>
2016-03-17 20:08:50 +0330

    HelloWorld sample for Jetty

---
+++ Jetty/HelloWorld/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Main extends AbstractHandler {
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        server.setHandler(new Main());
+
+        server.start();
+        server.join();
+    }
+
+    @Override
+    public void handle(String s, Request request, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException, ServletException {
+        httpServletResponse.setContentType("text/html;charset=utf-8");
+        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+        request.setHandled(true);
+        httpServletResponse.getWriter().println("<h1>Hello World</h1>");
+    }
+}
